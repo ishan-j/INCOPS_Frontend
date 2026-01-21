4bcb739718e8102cde323eaf658d241ad49dd771
ishan-j <ishanjoshi1001@gmail.com>
2026-01-22 02:37:15 +0530

    added ingress

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const res = await fetch("/auth/register", {
+      const res = await fetch("api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form)
